f4bfbda4a5f3fd3b30b5bb10d48b329f8b770e0d
Thamnoon boonmai <58143420@g.cmru.ac.th>
2019-07-14 10:33:20 +0700

    อัพใหม่

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+router.get('/create', function(req, res, next) {
+  res.render('create');
 });
 
 module.exports = router;
